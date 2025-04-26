8a0559a5e7eaa52c93cc4f3bce55fafd12310ddd
Selvaprakash P <selvaprakash0124@gmail.com>
2025-04-26 13:56:33 +0530

    Update script.js
    Reset

--- script.js
+++ script.js
@@ -16,17 +16,4 @@ document.addEventListener("DOMContentLoaded", () => {
             }
         });
     });
-
-    // Theme toggle functionality
-    const themeToggleBtn = document.getElementById("theme-toggle-btn");
-
-    if (themeToggleBtn) {
-        themeToggleBtn.addEventListener("click", () => {
-            document.body.classList.toggle("dark-theme");
-            document.querySelector("header").classList.toggle("dark-theme");
-            document.querySelectorAll("nav a").forEach(link => link.classList.toggle("dark-theme"));
-            document.querySelectorAll(".project, .experience-item").forEach(item => item.classList.toggle("dark-theme"));
-            document.querySelectorAll(".side-bar").forEach(side => side.classList.toggle("dark-theme"));
-        });
-    }
 });
